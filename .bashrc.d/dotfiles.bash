98d99f0fbcdde04248cfdd0d1de9cee7602ad6a1
Jonas Mauer <jam@kabelmail.net>
2019-06-29 03:08:26 +0200

    dotfiles-cap

--- .bashrc.d/dotfiles.bash
+++ .bashrc.d/dotfiles.bash
@@ -9,12 +9,14 @@ DOT_GIT=$DOT_PATH/.git
 
 alias dotfiles="/usr/bin/git --git-dir=$DOT_GIT --work-tree=$DOT_PATH/"
 
+# update
 function dotfiles-update() {
     dotfiles pull
     source ~/.bashrc
 }
 
-function dotfiles-push() {
+# commit and push
+function dotfiles-cap() {
     dotfiles add .
     dotfiles commit
     dotfiles push
